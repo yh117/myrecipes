efe60560e2f4086653cdadecd0d197a924a58b16
Yebin Han <yh117@duke.edu>
2015-11-07 23:35:30 +0000

    add image uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,13 @@ class Recipe < ActiveRecord::Base
   validates :summary,      presence: true, length: {maximum: 150, minimum: 10}
   validates :description , presence: true, length: {maximum: 500, minimum: 20}
   validates :chef_id,      presence: true
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        error.add(:picture, "should be less than 5MB")
+      end
+    end
 end
